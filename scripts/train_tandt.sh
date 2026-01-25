d28ac0b78b4a437fc760d02a6b04f31f225fec35
Yujia Lin <linyujia523@gmail.com>
2026-01-25 13:18:33 +0800

    ✨ feat: Add QAT flag in training scripts for improved performance

--- scripts/train_tandt.sh
+++ scripts/train_tandt.sh
@@ -6,12 +6,12 @@
 python train.py \
     -s data/tandt/train \
     -m output/train \
-    --eval \
+    --eval --qat \
     --pcd_path output/train/nerfacto/run/point_cloud.ply \
 
 # truck
 python train.py \
     -s data/tandt/truck \
     -m output/truck \
-    --eval \
+    --eval --qat \
     --pcd_path output/truck/nerfacto/run/point_cloud.ply \
