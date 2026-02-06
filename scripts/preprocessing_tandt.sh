1e94a6e123df1ebb1ffc3924ea15e8368249da1d
Yujia Lin <linyujia523@gmail.com>
2026-02-07 01:35:05 +0800

    🦄 refactor: Update preprocessing scripts to use relative paths for data and output directories

--- scripts/preprocessing_tandt.sh
+++ scripts/preprocessing_tandt.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/home/ubuntu/anaconda3/envs/locogs/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/home/ubuntu/anaconda3/envs/glowgs/lib:$LD_LIBRARY_PATH
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libcuda.so.1
 
 # train
 # processing data
-ns-process-data images --data ../data/tandt/train \
-                       --output-dir ../data/tandt/train \
+ns-process-data images --data data/tandt/train \
+                       --output-dir data/tandt/train \
                        --skip-colmap --skip-image-processing
 
 # fix trasform.json
 python fix_tandt_train.py
 
 # train nerfacto
-ns-train nerfacto --data ../data/tandt/train \
-                  --output-dir ../output \
+ns-train nerfacto --data data/tandt/train \
+                  --output-dir output \
                   --timestamp run \
                   --vis tensorboard \
                   --machine.seed 0  \
                   --pipeline.model.camera-optimizer.mode off
 
 # export pointcloud
-ns-export pointcloud --load-config ../output/train/nerfacto/run/config.yml \
-                     --output-dir ../output/train/nerfacto/run \
+ns-export pointcloud --load-config output/train/nerfacto/run/config.yml \
+                     --output-dir output/train/nerfacto/run \
                      --remove-outliers True \
                      --num-points 1000000 \
                      --normal-method open3d \
@@ -29,24 +29,24 @@ ns-export pointcloud --load-config ../output/train/nerfacto/run/config.yml \
 
 # truck
 # processing data
-ns-process-data images --data ../data/tandt/truck \
-                       --output-dir ../data/tandt/truck \
+ns-process-data images --data data/tandt/truck \
+                       --output-dir data/tandt/truck \
                        --skip-colmap --skip-image-processing
 
 # fix trasform.json
 python fix_tandt_truck.py
 
 # train nerfacto
-ns-train nerfacto --data ../data/tandt/truck \
-                  --output-dir ../output \
+ns-train nerfacto --data data/tandt/truck \
+                  --output-dir output \
                   --timestamp run \
                   --vis tensorboard \
                   --machine.seed 0  \
                   --pipeline.model.camera-optimizer.mode off
 
 # export pointcloud
-ns-export pointcloud --load-config ../output/truck/nerfacto/run/config.yml \
-                     --output-dir ../output/truck/nerfacto/run \
+ns-export pointcloud --load-config output/truck/nerfacto/run/config.yml \
+                     --output-dir output/truck/nerfacto/run \
                      --remove-outliers True \
                      --num-points 1000000 \
                      --normal-method open3d \
